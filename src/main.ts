d8cb880d2e3fc1c04146603df794b6eb5bb29f04
Taiki-nk <taikifate.0206@gmail.com>
2024-04-27 23:06:31 +0900

    fix:handlerのevent型定義

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { APIGatewayProxyEventV2 } from "https://deno.land/x/lambda/mod.ts";
 import { hello } from "./hello.ts";
 
-export const handler = async (event: any) => {
+export const handler = (event: APIGatewayProxyEventV2) => {
   console.log("event", JSON.stringify(event));
   return {
     statusCode: 200,
